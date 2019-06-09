0f7c1584ab77ec11757b5de28b9c5312095dc780
Luna <luna@MacBook-Pro.local>
2019-06-09 11:08:04 +0430

    Add New Files
    Start

---
+++ Stars/www/scripts/index.js
@@ -0,0 +1,36 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397705
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints,
+// and then run "window.location.reload()" in the JavaScript Console.
+var jquery_mobile_demo;
+(function (jquery_mobile_demo) {
+    "use strict";
+
+    (function (Application) {
+        function initialize() {
+            document.addEventListener('deviceready', onDeviceReady, false);
+        }
+        Application.initialize = initialize;
+
+        function onDeviceReady() {
+            // Handle the Cordova pause and resume events
+            document.addEventListener('pause', onPause, false);
+            document.addEventListener('resume', onResume, false);
+            // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+        }
+
+        function onPause() {
+            // TODO: This application has been suspended. Save application state here.
+        }
+
+        function onResume() {
+            // TODO: This application has been reactivated. Restore application state here.
+        }
+    })(jquery_mobile_demo.Application || (jquery_mobile_demo.Application = {}));
+    var Application = jquery_mobile_demo.Application;
+
+    window.onload = function () {
+        Application.initialize();
+    };
+})(jquery_mobile_demo || (jquery_mobile_demo = {}));
+//# sourceMappingURL=index.js.map
